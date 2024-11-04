c74a9f780f8741f5b010d22093f0a59d3976f07d
João Paulo Correia <ijoohn@hotmail.com>
2024-11-04 19:08:15 -0300

    adds uniqueness with scope

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
-  validates :phone, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: { scope: :proponent }
   validates :category, inclusion: { in: %w[personal reference] }
 
   belongs_to :proponent
